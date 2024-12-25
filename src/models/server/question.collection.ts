0712a3a39eca7382bed7a72c8a78047fdb5b78ce
Samir Khanal <gdssamir@gmail.com>
2024-12-25 11:24:40 +0545

    appwrite sdk setup

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,74 @@
+import { IndexType, Permission } from "node-appwrite";
+
+import { db, questionCollection } from "../name";
+import { databases } from "./config";
+
+export default async function createQuestionCollection() {
+  // create collection
+  await databases.createCollection(db, questionCollection, questionCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.create("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ]);
+  console.log("Question collection is created");
+
+  //creating attributes and Indexes
+
+  await Promise.all([
+    databases.createStringAttribute(db, questionCollection, "title", 100, true),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "content",
+      10000,
+      true,
+    ),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "authorId",
+      50,
+      true,
+    ),
+    databases.createStringAttribute(
+      db, //db id
+      questionCollection, //collectionid
+      "tags", //key
+      50, //character
+      true, //required
+      undefined, // default value
+      true, // array?
+    ),
+    databases.createStringAttribute(
+      db,
+      questionCollection,
+      "attachmentId",
+      50,
+      false, // required false
+    ),
+  ]);
+  console.log("Question Attributes created");
+
+  // create Indexes
+
+  await Promise.all([
+    databases.createIndex(
+      db,
+      questionCollection,
+      "title",
+      IndexType.Fulltext,
+      ["title"],
+      ["asc"],
+    ),
+    databases.createIndex(
+      db,
+      questionCollection,
+      "content",
+      IndexType.Fulltext,
+      ["content"],
+      ["asc"],
+    ),
+  ]);
+}
